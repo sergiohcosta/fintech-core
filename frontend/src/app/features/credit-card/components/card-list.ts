c7ade4f203603750de1451414f6157732e420415
Sergio Henrique Costa <sergiohcosta@gmail.com>
2026-01-27 18:46:07 -0300

    feat: Implement a generic confirmation dialog component and integrate it for credit card deletion.

--- frontend/src/app/features/credit-card/components/card-list.ts
+++ frontend/src/app/features/credit-card/components/card-list.ts
@@ -4,9 +4,12 @@ import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
+
 
 import { CreditCardService } from '../../../core/services/credit-card';
 import { CreditCardModel } from '../../../core/models/credit-card';
+import { ConfirmationDialogComponent } from '../../../components/confirmation-dialog/confirmation-dialog';
 
 import { Router, RouterLink } from '@angular/router';
 
@@ -31,6 +34,8 @@ export class CardListComponent implements OnInit {
   private service = inject(CreditCardService);
   cards = signal<CreditCardModel[]>([]);
 
+  private dialog = inject(MatDialog);
+
   displayedColumns: string[] = ['color', 'name', 'brand', 'limit', 'closingDay', 'actions'];
 
   ngOnInit(): void {
@@ -49,8 +54,24 @@ export class CardListComponent implements OnInit {
   }
 
   onDelete(card: CreditCardModel) {
-    if (confirm(`Excluir ${card.name}?`)) {
-      this.service.delete(card.id).subscribe(() => this.loadCards());
-    }
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '400px',
+      data: {
+        title: 'Excluir Cartão',
+        message: `Tem certeza que deseja remover o cartão "${card.name}"?`,
+        confirmText: 'Sim, excluir'
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.service.delete(card.id).subscribe({
+          next: () => {
+            this.loadCards();
+          },
+          error: (err) => console.error('Erro ao excluir', err)
+        });
+      }
+    });
   }
 }
